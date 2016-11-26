8c5c10fc6130c653ba2d522103c4c663ffb84a18
lkaleis <lindakaleis9@hotmail.com>
2016-11-25 23:19:40 -0500

    Update stage3_clean_mimic.R

--- R_scripts/stage3_clean_mimic.R
+++ R_scripts/stage3_clean_mimic.R
@@ -1,11 +1,10 @@
 #Stage 3: Data Cleaning & Pre-processing
-
 require(ggplot2)
 require(lattice)
 install.packages("corrplot")
 library(corrplot)
 
-#this is final file that was extracted for ICU patients from Postgres
+
 icustays <- read.csv(file ='D:/files/icupatients.csv')
 summary(icustays)
 
@@ -71,12 +70,51 @@ cor_matrix <- cor(icustays1[,c(6,8:16,18:32)])
 corrplot(cor_matrix, type ="lower", order="hclust", tl.col="black", tl.srt=27)
 corrplot(cor_matrix, type="lower", order="hclust", tl.col="black", tl.srt=27, method = "number", number.cex=0.7)
 
-#remove hemoglobin, meanbp_mean, and chloride_max due to high correlations
-icustays1 <- icustays1[,c(-12,-24,-26)]
-#remove ID attributes
-icustays1 <- icustays1[,c(-1,-2)]
+#features will be removed in Stage 4 scripts
+#END
 
-#visualize histograms for severity scores
-hist(icustays$sofa)
-hist(icustays$saps)
-hist(icustays$oasis)
+
+
+#Not using this code, but maybe for future
+
+# # mat : is a matrix of data
+# # ... : further arguments to pass to the native R cor.test function
+# cor.mtest <- function(mat, ...) {
+#   mat <- as.matrix(mat)
+#   n <- ncol(mat)
+#   p.mat<- matrix(NA, n, n)
+#   diag(p.mat) <- 0
+#   for (i in 1:(n - 1)) {
+#     for (j in (i + 1):n) {
+#       tmp <- cor.test(mat[, i], mat[, j], ...)
+#       p.mat[i, j] <- p.mat[j, i] <- tmp$p.value
+#     }
+#   }
+#   colnames(p.mat) <- rownames(p.mat) <- colnames(mat)
+#   p.mat
+# }
+# # matrix of the p-value of the correlation
+# p.mat <- cor.mtest(icustays1[,c(6,8:16,18:32)])
+# head(p.mat[, 1:5])
+# 
+# col <- colorRampPalette(c("#BB4444", "#EE9988", "#FFFFFF", "#77AADD", "#4477AA"))
+# corrplot(cor_matrix, method="color", col=col(200),number.cex=0.5,type="upper", order="hclust", addCoef.col = "black", # Add coefficient of correlation
+#          tl.col="black", tl.srt=45, #Text label color and rotation
+#          # Combine with significance
+#          p.mat = p.mat, sig.level = 0.01, insig = "blank", 
+#          # hide correlation coefficient on the principal diagonal
+#          diag=FALSE 
+# )
+
+# #remove highly correlated features 
+# install.packages("mlbench")
+# install.packages("caret")
+# library(caret)
+# set.seed(7)
+# corrmatrix <- cor(icustays1[,c(6,8:16,18:32)])
+# print(corrmatrix)
+# corrplot(corrmatrix, method="number", p.mat=p.mat, sig.level = 0.5, type="upper", order="hclust", tl.col="black")
+# highcorr <- findCorrelation(corrmatrix, cutoff=0.75)
+# print(highcorr)
+
+
